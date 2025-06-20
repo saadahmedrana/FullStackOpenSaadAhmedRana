4fe8d9afdbfe25f5cf3ac782fbdd654bf135d62d
Saad Ahmed <sadisfaction570@gmail.com>
2025-06-20 12:36:34 +0300

    1.3

---
+++ part 1/1.3/src/App.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+
+const Header = (props) => {
+  return <h1>{props.course}</h1>
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  )
+}
+
+const Content = ({ part1, part2, part3 }) => {
+  return (
+    <div>
+      <Part name={part1.name} exercises={part1.exercises} />
+      <Part name={part2.name} exercises={part2.exercises} />
+      <Part name={part3.name} exercises={part3.exercises} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return <p>Number of exercises {props.total}</p>
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  const total = part1.exercises + part2.exercises + part3.exercises
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        part2={part2}
+        part3={part3}
+      />
+      <Total total={total} />
+    </div>
+  )
+}
+
+export default App
